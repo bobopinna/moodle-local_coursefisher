936c2dcb73ba4fcb536b80e9b2c42a2ae6dd0d10
Roberto <bobo@di.unipmn.it>
2022-11-24 16:55:49 +0100

    Fixed unenrol link

--- lib.php
+++ lib.php
@@ -72,15 +72,14 @@ function local_coursefisher_extend_settings_navigation(settings_navigation $nav,
                                     require_once($CFG->dirroot . '/enrol/' . $metacourseinstance->enrol . '/lib.php');
                                     $enrolclassname = 'enrol_' . $metacourseinstance->enrol . '_plugin';
                                     $enrol = new $enrolclassname();
-                                    if ($enrol->get_unenrolself_link($metacourseinstance) != null) {
+                                    $unenrolink = $enrol->get_unenrolself_link($metacourseinstance);
+                                    if ($unenrolink != null) {
                                         $query = array('enrolid' => $metacourseinstance->id, 'userid' => $USER->id);
                                         if ($userenrolment = $DB->get_record('user_enrolments', $query)) {
                                             $enrolledbefore = $userenrolment->timestart < time();
                                             $stillenrolled = ($userenrolment->timeend == 0) || ($userenrolment->timeend < time());
                                             if ($enrolledbefore && $stillenrolled) {
                                                 $node = $nav->get('courseadmin');
-                                                $unenrolink = new moodle_url('/enrol/' . $metacourseinstance->enrol .
-                                                              '/unenrolself.php', array('enrolid' => $metacourseinstance->id));
                                                 $unenrolstr = get_string('unenrolme', 'enrol', $metacourse->shortname);
                                                 $icon = new pix_icon('i/user', '');
                                                 $node->add($unenrolstr, $unenrolink, navigation_node::TYPE_USER, null, null, $icon);
