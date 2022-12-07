b532065ccbacee53b8c739e844e6889a8c828c55
Roberto <bobo@di.unipmn.it>
2022-12-07 17:17:49 +0100

    Fixed Moodle minimum request

--- backend/json/version.php
+++ backend/json/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017022600;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2010112400;        // Requires this Moodle version.
+$plugin->version   = 2022043000;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2018051700;        // Requires this Moodle version.
 $plugin->component = 'coursefisherbackend_json'; // Full name of the plugin (used for diagnostics).
